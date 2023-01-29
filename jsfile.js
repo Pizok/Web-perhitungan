5f91bad80ce84bd1a69c8b1a78f9deb08150fe99
Pizok <orangganteng1140@gmail.com>
2023-01-30 00:40:08 +0700

    commit

--- jsfile.js
+++ jsfile.js
@@ -1,6 +1,7 @@
 function cdr() {
-    let d = document.getElementById("ckematian").value;
-    let p = document.getElementById("cPenduduk").value;
+    let d = number(document.getElementById("ckematian").value);
+    let p = number(document.getElementById("cPenduduk").value);
     let result = d / p * 1000;
-    document.getElementById("cresult").innerHTML = result;
+    document.getElementById("result").innerHTML = result;
 }
+document.getElementById("result").innerHTML = "dsdsds";
