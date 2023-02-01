f26b3b12805f56759634322103a9a919517f6745
Pizok <123370367+Pizok@users.noreply.github.com>
2023-02-01 10:51:12 +0700

    com

--- jsfile.js
+++ jsfile.js
@@ -2,6 +2,13 @@ function cdr() {
     let d = number(document.getElementById("ckematian").value);
     let p = number(document.getElementById("cPenduduk").value);
     let result = d / p * 1000;
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("cresult").innerHTML = result;
+}
+
+function asbr() {
+    let d = number(document.getElementById("akematian").value);
+    let p = number(document.getElementById("aPenduduk").value);
+    let result = d / p * 1000;
+    document.getElementById("aresult").innerHTML = result;    
 }
 document.getElementById("result").innerHTML = "dsdsds";
